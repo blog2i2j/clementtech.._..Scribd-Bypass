146f8241d9223526727ae7424fe273e9fbdd32a4
Clement <119944151+sussynk@users.noreply.github.com>
2026-03-10 13:19:07 +0800

    rename var

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const scribd_link = document.getElementById("scribd_link");
+const form = document.getElementById("form");
 
-console.log(scribd_link);
 
-const link_format = new URL(scribd_link);
 
-console.log(link_format);
+// console.log(scribd_link);
+
+// const link_format = new URL(scribd_link);
+
+// console.log(link_format);
 
 // const scribd_splitted_link = scribd_link.split("/");
 
