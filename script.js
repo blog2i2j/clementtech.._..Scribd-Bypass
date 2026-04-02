40f0e041eb094b09f7fcd140f752f8aafac09c1f
Clement Chong <119944151+sussynk@users.noreply.github.com>
2026-04-02 22:50:35 +0800

    working now

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const form = document.getElementById("form");
 
 const output = document.getElementById("output");
 
+const outputLink = document.getElementById("outputLink");
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
@@ -9,5 +11,9 @@ form.addEventListener("submit", function (event) {
 
   const scribd_splitted_link = scribd_link.split("/")[4];
 
-  output.textContent = `https://www.scribd.com/embeds/${scribd_splitted_link}/content?start_page=1&view_mode=scroll`;
+  const link_ready = `https://www.scribd.com/embeds/${scribd_splitted_link}/content?start_page=1&view_mode=scroll`;
+
+  output.textContent = link_ready;
+
+  outputLink.href = link_ready;
 });
