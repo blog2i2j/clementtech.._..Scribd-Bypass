cb95bde0fee548e4411ab033eca57aac9a46cd53
Clement Chong <119944151+sussynk@users.noreply.github.com>
2026-04-02 22:29:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const form = document.getElementById("form");
 const output = document.getElementById("output");
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const scribd_link = document.getElementById("scribd_link").value.trim();
+  const scribd_link = document.getElementById("scribd_link").value.trim();
 
-    output.textContent = `https://www.scribd.com/embeds/${scribd_link}/content?start_page=1&view_mode=scroll`;
+  const scribd_splitted_link = scribd_link.split("/")[4];
+
+  output.textContent = `https://www.scribd.com/embeds/${scribd_splitted_link}/content?start_page=1&view_mode=scroll`;
 });
